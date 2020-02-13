43c42f39c9973e0818fff6a8fc2740408b81b3cc
rednaxela700 <ohleg@windowslive.com>
2020-02-13 23:42:44 +0100

    added items to $cars array

--- 1.variables/arrays.php
+++ 1.variables/arrays.php
@@ -8,3 +8,8 @@
 */
 $people = ['Kevin', 'James', 'Nicole', 'Sara'];
 echo $people[1];
+$numbers = [23, 55, 12];
+$cars = ['Honda', 'Toyota', 'Ford'];
+echo $cars[2];
+$cars[3] = 'Chevy'; // added another car to array
+$cars[] = 'BWM'; // added bmw as last item of the array
