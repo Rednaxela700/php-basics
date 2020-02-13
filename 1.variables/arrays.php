22d6d6b5d272f25329c5cdd2b900afa30ba50111
rednaxela700 <ohleg@windowslive.com>
2020-02-13 23:46:36 +0100

    added count(), print_r() and var_dump() fn's

--- 1.variables/arrays.php
+++ 1.variables/arrays.php
@@ -13,3 +13,9 @@ $cars = ['Honda', 'Toyota', 'Ford'];
 echo $cars[2];
 $cars[3] = 'Chevy'; // added another car to array
 $cars[] = 'BWM'; // added bmw as last item of the array
+
+echo count($cars); // outputs array length
+echo '<br>';
+print_r($cars); //prints whole array as string
+echo '<br>';
+var_dump($cars); //prints whole array as string including data types
