0fe93e409f0f43bd744e4cfcfb53f67ae93aaaf1
rednaxela700 <ohleg@windowslive.com>
2020-03-02 16:05:43 +0100

    added switch statement

--- 1.variables/conditionalsAndComparison.php
+++ 1.variables/conditionalsAndComparison.php
@@ -39,3 +39,22 @@ if($num === '5') {
  * or - ||
  * xor - exclusive comparison - only if left or right is true, but not both
  */
+
+//switches
+/*
+ *  test for a value with several conditions
+ */
+
+$favColor = "red";
+
+switch ($favColor){
+    case 'red':
+        echo 'your favourite color is red';
+        break;
+    case 'blue':
+        echo 'your favourite color is blue';
+        break;
+    default:
+        echo "your favourite color is different than red or blue";
+
+}
