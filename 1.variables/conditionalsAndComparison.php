097f1e6da1dd393ec95f4f2b8b200669b29ab671
rednaxela700 <ohleg@windowslive.com>
2020-03-02 15:59:48 +0100

    added elseif and else

--- 1.variables/conditionalsAndComparison.php
+++ 1.variables/conditionalsAndComparison.php
@@ -26,4 +26,8 @@ if($num == '5') {
 }
 if($num === '5') {
     echo "<br> passed strict comparison";
+} elseif ($num === 5) {
+    echo "<br> $num is a number equal to 5";
+} else {
+    echo "num else log";
 }
