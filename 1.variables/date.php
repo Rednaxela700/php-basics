1c5579badf3e32f72eaf1d3e282fd5af407c3be1
rednaxela700 <ohleg@windowslive.com>
2020-03-06 12:14:18 +0100

    added timestamps with time set in strings

--- 1.variables/date.php
+++ 1.variables/date.php
@@ -18,8 +18,19 @@ echo date('a'); //AM/PM
 
 //make a timestamp in unix time
 
-$timestamp = mktime(10,14,54,01,02,1993);
+$timestamp = mktime(10, 14, 54, 01, 02, 1993);
 
-echo "<br>".$timestamp."<br>";
+echo "<br>" . $timestamp . "<br>";
 
 echo date('m/d/Y', $timestamp); //date fn can take timestamp as second parameter
+
+//convert time typed in string to unix format
+$timestamp2 = strtotime('7:00pm March 06 2020');
+
+echo "<br>" . $timestamp2 . "<br>";
+
+echo date('m/d/Y h:i:sa', $timestamp2);
+
+$timestamp3 = ('tomorrow');
+
+echo "<br>" . date('m/d/Y h:i:sa', $timestamp3) . "<br>";
