7ab0443470cba9859a771523d7021ec13b505ae1
rednaxela700 <ohleg@windowslive.com>
2020-03-06 11:50:19 +0100

    set default timezone, echo hours, minutes, seconds

--- 1.variables/date.php
+++ 1.variables/date.php
@@ -3,3 +3,14 @@ echo date('d'); // Day
 echo date('m'); // Month
 echo date('Y'); // Year
 echo date('l'); // day of the week
+
+echo '<br>';
+echo date('Y/m/d'); // year/month.day
+echo '<br>';
+
+//    Set timezone
+date_default_timezone_set('Europe/Warsaw');
+
+echo date('h'); //hours
+echo date('i'); //minutes
+echo date('s'); //seconds
