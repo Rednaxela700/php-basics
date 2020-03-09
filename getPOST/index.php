4e0280f3db74011436d1d00d451bf635e61bca3c
rednaxela700 <ohleg@windowslive.com>
2020-03-09 19:35:53 +0100

    echo data from form on submit, added bootstrap styling

--- getPOST/index.php
+++ getPOST/index.php
@@ -1,4 +1,8 @@
-<?php ?>
+<?php
+if(isset($_GET['name'])) {
+    echo htmlentities($_GET['name'].'<br>'.$_GET['email']);
+}
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -13,13 +17,13 @@
 <div class="row">
     <div class="col-sm-6">
     <form action="index.php" method="get" class="container">
-        <div class="input-group-prepend">
-            <label for="name" class="col-form-label">Name</label><input type="text" name="name" class="input-group">
+        <div class="input-group-prepend m-4">
+            <label for="name" class="col-form-label px-2">Name</label><input type="text" name="name" class="input-group px-2">
         </div>
-        <div class="input-group-prepend">
-            <label for="email" class="col-form-label">Email</label><input type="email" name="email" class="input-group">
+        <div class="input-group-prepend m-4">
+            <label for="email" class="col-form-label px-2">Email</label><input type="email" name="email" class="input-group px-2">
         </div>
-        <button class="btn">submit</button>
+        <button class="btn btn-primary d-block mx-auto">submit</button>
     </form>
     </div>
 </div>
