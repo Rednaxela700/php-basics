91349c8eb758a6e1a3b6748afc530d639f2c1190
rednaxela700 <ohleg@windowslive.com>
2020-03-09 19:47:10 +0100

    changed post to $_REQUEST, works for get and post

--- getPOST/index.php
+++ getPOST/index.php
@@ -1,6 +1,6 @@
 <?php
-if(isset($_POST['name'])) {
-    echo htmlentities($_POST['name'].'<br>'.$_POST['email']); //html wont work when typed
+if (isset($_REQUEST['name'])) {
+    echo htmlentities($_REQUEST['name'] . '<br>' . $_REQUEST['email']); //html wont work when typed
 }
 ?>
 <!doctype html>
@@ -16,15 +16,17 @@ if(isset($_POST['name'])) {
 <body>
 <div class="row">
     <div class="col-sm-6">
-    <form action="index.php" method="post" class="container">
-        <div class="input-group-prepend m-4">
-            <label for="name" class="col-form-label px-2">Name</label><input type="text" name="name" class="input-group px-2">
-        </div>
-        <div class="input-group-prepend m-4">
-            <label for="email" class="col-form-label px-2">Email</label><input type="email" name="email" class="input-group px-2">
-        </div>
-        <button class="btn btn-primary d-block mx-auto">submit</button>
-    </form>
+        <form action="index.php" method="post" class="container">
+            <div class="input-group-prepend m-4">
+                <label for="name" class="col-form-label px-2">Name</label><input type="text" name="name"
+                                                                                 class="input-group px-2">
+            </div>
+            <div class="input-group-prepend m-4">
+                <label for="email" class="col-form-label px-2">Email</label><input type="email" name="email"
+                                                                                   class="input-group px-2">
+            </div>
+            <button class="btn btn-primary d-block mx-auto">submit</button>
+        </form>
     </div>
 </div>
 
